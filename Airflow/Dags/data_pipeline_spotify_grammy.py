277d167d05ed2eaadc9b6ea602ac391534d95fd1
camilodelgado23 <cami@LAPTOP-J7426DRI>
2024-10-04 22:06:22 -0500

    commit final

--- Airflow/Dags/data_pipeline_spotify_grammy.py
+++ Airflow/Dags/data_pipeline_spotify_grammy.py
@@ -15,6 +15,7 @@ from Airflow.carga.carga_Spotify import main as carga_spotify
 from Airflow.Union.merge_dataset import seleccionar_spotify, seleccionar_grammys, unir_datasets
 from Airflow.carga.carga_merge import main as carga_merge
 from Base_de_Datos_Operaciones.conexionBD import create_connection
+from Airflow.carga.subir_drive import upload_to_drive  
 
 # Definir argumentos por defecto del DAG
 default_args = {
@@ -31,22 +32,19 @@ dag = DAG(
     'spotify_grammy_data_pipeline',
     default_args=default_args,
     description='Pipeline de datos para Spotify y Grammys',
-    schedule_interval=None,  # Aquí defines el intervalo de ejecución. None es para ejecución manual
-    start_date=days_ago(1),  # La fecha de inicio se ajusta a ayer
+    schedule_interval=None,
+    start_date=days_ago(1),
     catchup=False,
 )
 
 # 1. Tarea: Cargar el dataset de Spotify en la base de datos y seleccionar los datos
 def cargar_y_seleccionar_spotify():
-    # Cargar el CSV en la base de datos
-    carga_spotify('workshop2/csv/spotify_dataset.csv')  # Asegúrate de que la función carga_spotify esté implementada para esto
-    
-    # Crear conexión y seleccionar datos de la tabla
+    carga_spotify('workshop2/csv/spotify_dataset.csv')  
     connection = create_connection()
     spotify_df = seleccionar_spotify(connection, 'Spotify')
     connection.close()
     
-    return spotify_df  # Retornar el DataFrame
+    return spotify_df  
 
 cargar_spotify_task = PythonOperator(
     task_id='cargar_y_seleccionar_csv_spotify',
@@ -54,14 +52,13 @@ cargar_spotify_task = PythonOperator(
     dag=dag,
 )
 
-# 2. Tarea: Transformar el dataset de Spotify (eliminación de columnas)
+# 2. Tarea: Transformar el dataset de Spotify
 def transformacion_spotify_dataset(ti):
-    spotify_df = ti.xcom_pull(task_ids='cargar_y_seleccionar_csv_spotify')  # Obtener el DataFrame de la tarea anterior
+    spotify_df = ti.xcom_pull(task_ids='cargar_y_seleccionar_csv_spotify')
     
     if spotify_df is not None:
-        # Eliminación de columnas innecesarias
         spotify_df = spotify_df.drop(columns=['time_signature', 'key'], errors='ignore')
-        return spotify_df  # Retornar el DataFrame transformado
+        return spotify_df  
 
 transformacion_spotify_task = PythonOperator(
     task_id='transformacion_dataset_spotify',
@@ -82,7 +79,7 @@ extraccion_grammys_task = PythonOperator(
     dag=dag,
 )
 
-# 4. Tarea: Transformar el dataset de Grammys (eliminación de columnas innecesarias)
+# 4. Tarea: Transformar el dataset de Grammys
 def transformacion_grammys_dataset():
     connection = create_connection()
     grammys_df = seleccionar_grammys(connection, 'Grammys')
@@ -101,14 +98,12 @@ transformacion_grammys_task = PythonOperator(
 # Tarea: Unir los datasets de Spotify y Grammys
 def unir_datasets_spotify_grammys():
     connection = create_connection()
-
-    # Seleccionar los datasets de Spotify y Grammys directamente de la base de datos
     spotify_df = seleccionar_spotify(connection, 'Spotify')
     grammys_df = seleccionar_grammys(connection, 'Grammys')
 
     if spotify_df is not None and grammys_df is not None:
         merged_df = unir_datasets(spotify_df, grammys_df)
-        merged_df.to_csv('workshop2/csv/mergedg_dataset.csv', index=False)  # Guardar dataset unido
+        merged_df.to_csv('workshop2/csv/mergedg_dataset.csv', index=False)
     else:
         print("No se pudieron obtener los DataFrames para la unión.")
 
@@ -120,10 +115,12 @@ unir_datasets_task = PythonOperator(
     dag=dag,
 )
 
-# Tarea: Cargar el dataset unido en la base de datos
+# Tarea: Cargar el dataset unido en la base de datos y en Google Drive
 def cargar_merge_data():
-    file_path = 'workshop2/csv/mergedg_dataset.csv'  # Ruta del archivo CSV generado
-    carga_merge.main(file_path)  # Llama a la función main de carga_merge
+    file_path = 'workshop2/csv/mergedg_dataset.csv'
+    carga_merge.main(file_path)
+
+    upload_to_drive(file_path)  
 
 cargar_merge_task = PythonOperator(
     task_id='cargar_merge_datos',
@@ -131,7 +128,5 @@ cargar_merge_task = PythonOperator(
     dag=dag,
 )
 
-# Definir dependencias entre las tareas
 cargar_spotify_task >> transformacion_spotify_task >> unir_datasets_task >> cargar_merge_task
-extraccion_grammys_task >> transformacion_grammys_task >> unir_datasets_task
-
+extraccion_grammys_task >> transformacion_grammys_task >> unir_datasets_task  
